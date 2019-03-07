567fb9e4c2459cb8a3e932952465a06dc1a4a026
Hectorsalcedo <47277340+Hectorsalcedo@users.noreply.github.com>
2019-03-06 21:03:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 var express = require('express');
 var socket = require('socket.io');
 
-//App setup
+// Heroku won't actually allow us to use WebSockets
+// so we have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+//socket.configure(function () { 
+//  socket.set("transports", ["xhr-polling"]); 
+//  socket.set("polling duration", 10); 
+//});
+
+// App setup
 var app = express();
 var port = process.env.PORT || 5000;
 //var server = app.listen(4000, function(){
-//  console.log('listening to requests on port 4000');
-//});
+//    console.log('listening for requests on port 4000,');
+//})
 var server = app.listen(port, function(){
     console.log('listening for requests on port ' + port);
   });
 
-//Static files
+// Static files
 app.use(express.static('public'));
 var io = require('socket.io').listen(server);
 
 // Socket setup & pass server
+//var io = require('socket.io').listen(server);
 //var io = socket(server);
 io.on('connection', (socket) => {
 
@@ -25,9 +34,11 @@ io.on('connection', (socket) => {
     socket.on('chat', function(data){
         // console.log(data);
         io.sockets.emit('chat', data);
-  });
+    });
 
-  socket.on('escribiendo',function(data){
-    socket.broadcast.emit('escribiendo',data);
-  });
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
