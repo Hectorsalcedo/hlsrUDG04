4334e536c869cf38429f6ceb84ee07ba5fb0999c
Hectorsalcedo <47277340+Hectorsalcedo@users.noreply.github.com>
2019-03-06 21:04:35 -0600

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,34 +1,33 @@
 // Make connection
-//var socket = io.connect('http://localhost:4000');
 var socket = io.connect(window.location.hostname);
+//var socket = io.connect('http://localhost:4000');
 
-//Query DOM
-var message = document.getElementById('message');
-var handle = document.getElementById('handle');
-var btn = document.getElementById('send');
-var output = document.getElementById('output');
-var feedback = document.getElementById('feedback');
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
 
-//Emit events
-
+// Emit events
 btn.addEventListener('click', function(){
-  socket.emit('chat', {
-      message: message.value,
-      handle: handle.value
-  });
-  message.value = "";
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+    message.value = "";
 });
 
-message.addEventListener('keypress',function(){
-  socket.emit('escribiendo',handle.value);
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
 })
 
 // Listen for events
 socket.on('chat', function(data){
-  feedback.innerHTML = "";
-  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
 
-socket.on('escribiendo',function(data){
-  feedback.innerHTML = '<p><em>' + data + ' esta escribiendo un mensage...</em></p>';
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' está escribiendo...</em></p>';
 });
